a68e55c9bc15aa82903ac8ef06ac224ee6dbd124
Jefferson Huang <hsiaobei@gmail.com>
2018-02-09 10:30:12 -0800

    initial commit

---
+++ MDP.py
@@ -0,0 +1,125 @@
+from __future__ import division
+from math import *
+from collections import defaultdict
+import numpy as np
+import random
+import time
+
+class MDP:
+    def __init__(self, mdp_dict):
+        """
+        Input: a dictionary representation of the MDP; the keys are state-action 
+               pairs, and the values are pairs whose first element is the 
+               corresponding one-step reward and whose second element is a 
+               dictionary where each key is a state and each value is the 
+               probability of transitioning to that state
+
+        Attributes:
+        m = number of state-action pairs
+        n = number of states
+        X = list of states
+        A = dictionary of lists of available actions; keys are states, values are 
+            sets of actions
+        r = dictionary of one-step rewards; keys are state-action pairs, values 
+            are rewards
+        p = dictionary of transition probabilities; keys are state-action pairs, 
+            values are dictionaries where each key is a state and the corresponding 
+            value is the probability of transitioning to that state
+        """
+        self.m = len(mdp_dict)
+        
+        states = set()
+        for state_action_pair in mdp_dict.keys():
+            states.add(state_action_pair[0])
+        self.n = len(states)        
+        self.X = list(states)
+        
+        actions = {}
+        for state in states:
+            act_set = set()
+            for key in mdp_dict:
+                if key[0] == state:
+                    act_set.add(key[1])
+            actions[state] = list(act_set)
+        self.A = actions
+
+        rewards = {}
+        for state_action_pair in mdp_dict.keys():
+            rewards[state_action_pair] = mdp_dict[state_action_pair][0]
+        self.r = rewards
+
+        transition_probs = {}
+        for state_action_pair in mdp_dict.keys():
+            transition_probs[state_action_pair] = mdp_dict[state_action_pair][1]
+        self.p = transition_probs
+
+def policy_eval(pi, mdp, discount_factor):
+    """ Generate the transition matrix. """
+    P_pi = np.fromfunction(np.vectorize(lambda i,j : mdp.p[mdp.X[i],
+               pi[mdp.X[i]]][mdp.X[j]]), (mdp.n, mdp.n), dtype=int)
+    """ Generate the reward vector. """
+    r_pi = np.fromfunction(np.vectorize(lambda i,j : mdp.r[mdp.X[i],
+               pi[mdp.X[i]]]), (mdp.n, 1), dtype=int)
+    return np.linalg.inv(np.eye(mdp.n) - discount_factor * P_pi).dot(r_pi)
+
+def greedy_action(state, v, mdp, discount_factor):
+    state_action_values = [(mdp.r[state, action] +
+                            discount_factor*sum([mdp.p[state, action][mdp.X[j]] * v[j]
+                                for j in range(len(mdp.X))]))[0]
+                            for action in mdp.A[state]]
+    return mdp.A[state][np.argmax(state_action_values)]
+
+def greedy_policy(v, mdp, discount_factor):
+    # print 'start greedy policy'
+    pi = {}
+    for state in mdp.X:
+        pi[state] = greedy_action(state, v, mdp, discount_factor)
+    return pi
+
+def optimality_operator(v, mdp, discount_factor):
+    pi = greedy_policy(v, mdp, discount_factor)
+    # print 'got greedy policy'
+    Tv = np.zeros((mdp.n, 1))
+    for i in range(mdp.n):
+        Tv[i] = mdp.r[mdp.X[i], pi[mdp.X[i]]] + discount_factor*sum([mdp.p[mdp.X[i], pi[mdp.X[i]]][mdp.X[j]]*v[j] for j in range(mdp.n)])
+    return Tv
+
+def policy_iteration(mdp, discount_factor):
+    start = time.time()
+    """Select an initial policy."""
+    pi = {state : random.choice(mdp.A[state]) for state in mdp.X}
+    iteration_count = 0
+    while True:
+        iteration_count += 1
+        """Evaluate the current policy."""
+        v_pi = policy_eval(pi, mdp, discount_factor)
+        """Try to improve the current policy."""
+        updated = False
+        for state in mdp.X:
+            a_star = greedy_action(state, v_pi, mdp, discount_factor)
+            if a_star != pi[state]:
+                pi[state] = a_star
+                updated = True
+        if not(updated):
+            print 'Number of policy iterations: {}'.format(iteration_count)
+            print 'Policy iteration took {} seconds.'.format(time.time() - start)
+            return pi
+
+def value_iteration(mdp, discount_factor, epsilon=0.001):
+    start = time.time()
+    """Initialization"""
+    v = np.zeros((mdp.n, 1))
+    # print 'initialized value iteration'
+    Tv = optimality_operator(v, mdp, discount_factor)
+    # print 'did first value iteration'
+    """Iterate until epsilon-convergence"""
+    iteration_count = 0
+    while max(np.absolute(Tv - v))[0] > epsilon*(1 - discount_factor)/discount_factor:
+        iteration_count += 1
+        # print 'Iteration {}'.format(iteration_count)
+        v = Tv
+        Tv = optimality_operator(v, mdp, discount_factor)
+    print 'Number of value iterations: {}'.format(iteration_count)
+    print 'Value iteration took {} seconds.'.format(time.time() - start)
+    pi = greedy_policy(v, mdp, discount_factor)
+    return pi
---
+++ queues.py
@@ -0,0 +1,109 @@
+from __future__ import division
+from math import *
+from collections import defaultdict
+from sklearn.cluster import KMeans
+import numpy as np
+import random
+import MDP
+import time
+
+class CRW:
+    """
+        Single-server queue with a single customer class, modeled as a discrete
+        -time controlled random walk. At most one event (i.e., an arrival or 
+        service completion) can occur during each time slot.
+
+        Attributes:
+        buffer_size = maximum number of customers (including any in service) that 
+        can be in the system
+        service_probs = list of possible service probabilities
+        arrival_prob = probability that there's an arrival
+        cost_func = function whose input is (x, q) and whose output is the cost 
+                    incurred when the current state is x and the service 
+                    probability q is used
+    """
+    def __init__(self, N, A, p, g):
+        self.buffer_size = N
+        self.service_probs = A
+        self.arrival_prob = p
+        self.cost_func = g
+
+    def generate_mdp_dict(self):
+        mdp_dict = {}
+        p = self.arrival_prob
+        for a in self.service_probs:
+            mdp_dict[(0, a)] = (-self.cost_func(0, a),
+                                    defaultdict(lambda : 0, {0 : 1-p, 1 : p}))
+            for x in range(1, self.buffer_size):
+                mdp_dict[(x, a)] = (-self.cost_func(x, a),
+                                        defaultdict(lambda : 0, {x-1 : a, x : 1-p-a, x+1 : p}))
+            mdp_dict[(self.buffer_size, a)] = (-self.cost_func(self.buffer_size, a), defaultdict(lambda : 0, {self.buffer_size-1 : a, self.buffer_size : 1-a}))
+        return mdp_dict
+
+    def compute_optimal_policy(self, discount_factor):
+        mdp_dict = self.generate_mdp_dict()
+        mdp = MDP.MDP(mdp_dict)
+        pi = MDP.policy_iteration(mdp, discount_factor)
+        return pi
+
+    def simulate(self, state, service_prob):
+        """Simulate the queueing model for one step.
+
+           state = current state
+           service_prob = probability of a service completion 
+        """
+        u = np.random.uniform()
+        if state == 0:
+            if u < self.arrival_prob:
+                return (self.cost_func(state, service_prob), state+1)
+            else:
+                return (self.cost_func(state, service_prob), state)
+        elif state > 0 and state < self.buffer_size-1:
+            if u < self.arrival_prob:
+                return (self.cost_func(state, service_prob), state+1)
+            elif u < self.arrival_prob + service_prob:
+                return (self.cost_func(state, service_prob), state-1)
+            else:
+                return (self.cost_func(state, service_prob), state)
+        elif state == self.buffer_size:
+            if u < service_prob:
+                return (self.cost_func(state, service_prob), state-1)
+            else:
+                return (self.cost_func(state, service_prob), state)
+
+    def simulate_test(self, x, q, n):
+        """Check that the simulator is providing transitions at the
+           right frequencies.
+        """
+        n_down = 0
+        n_stay = 0
+        n_up = 0
+        for i in range(n):
+            (r, y) = self.simulate(x, q)
+            if y == x-1:
+                n_down += 1
+            elif y == x:
+                n_stay += 1
+            elif y == x+1:
+                n_up += 1
+        return (n_down/n, n_stay/n, n_up/n)
+
+    def run_random_policy(self, init_state, num_steps):
+        """Run the policy that selects actions uniformly at random, starting 
+           from a given initial state.
+
+           init_state = initial state
+           num_steps = number of steps to run the policy
+        """
+        sample_transitions = {}
+        for q in self.service_probs:
+            sample_transitions[q] = []
+        current_state = init_state
+        for i in range(num_steps):
+            service_prob = random.choice(self.service_probs)
+            (cost, next_state) = self.simulate(current_state, service_prob)
+            """Assume that the RL algorithm will maximize rewards."""
+            sample_transitions[service_prob].append((current_state,
+                                                         -cost, next_state))
+            current_state = next_state
+        return sample_transitions
